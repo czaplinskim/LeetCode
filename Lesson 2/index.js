1c325e81212bbe7901952854ed394f975588b8e7
Michał Czapliński <czaplinskim@icloud.com>
2023-06-07 16:14:03 +0200

    Lesson 1 and 2

---
+++ Lesson 2/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+
+var isPalindrome = function(x) {
+
+    const array = Array.from(String(x), Number);
+    let trueCounter = 0
+
+    for (let i = 0; i < array.length; i++) {
+        
+        const reversedIndex = array.length - i - 1
+        if(array[i] == array[reversedIndex]){ trueCounter++ }
+
+    }
+
+    return (trueCounter == array.length ? true : false)
+    
+};
+
+
+console.log(isPalindrome(122221))
