e8b2a22a345c85ecfd436d2d164cf0332657cc32
Michał Czapliński <czaplinskim@icloud.com>
2023-06-08 15:56:57 +0200

    Lesson 4

---
+++ src/Lesson 4/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+
+    let i = init;
+
+    return {
+        increment: () => { return ++init },
+        decrement: () => { return --init },
+        reset: () => { return init = i }
+    }
+    
+};
+
+const counter = createCounter(0);
+console.log(counter.increment()); // 1
+console.log(counter.increment()); // 2
+console.log(counter.decrement()); // 1
+console.log(counter.reset()); // 0
+console.log(counter.reset()); // 0
+
