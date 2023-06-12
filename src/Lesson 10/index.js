98b0754b7a2c20d2f63e2a3194bd580c473a80bb
Michał Czapliński <czaplinskim@icloud.com>
2023-06-12 18:40:54 +0200

    Lesson 11

---
+++ src/Lesson 10/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums, s = nums[0]) {
+
+    return nums.reduce((p,c,i) =>((c+1 !== nums[i+1] ? p.push(s === c ? `${c}` : `${s}->${c}`) : '') && (s = nums[i+1]), p), [])
+
+
+};
+
+const nums1 = [0,1,2,4,5,7]
+
+console.log(summaryRanges(nums1))
