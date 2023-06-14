f3783efd728a6fce03727fc3e51700ea89c4525c
Michał Czapliński <czaplinskim@icloud.com>
2023-06-14 10:38:59 +0200

    Lesson 13

--- src/Lesson 13/index.js
+++ src/Lesson 13/index.js
@@ -7,54 +7,6 @@
  */
 var equalPairs = function(grid) {
 
-
-    // let newArray = []
-    // let newArray2 = []
-
-    // for(let i = 0; i < grid.length; i++ ) {
-
-    //     let tempArray = []
-      
-    //         for(let n = 0; n < grid[i].length; n++) {
-
-    //             tempArray.push(grid[n][i])
-    //         }
-
-    //     newArray.push(tempArray.join())
-    //     newArray2.push(grid[i].join())
-    // }
-
-    // newArray = [...newArray, ...newArray2]
-
-    // let counts = newArray.filter((element, index) => {
-    // return newArray.indexOf(element) == index; });
-     
-    // return newArray.length - counts.length
-
-
-
-    // for(let i = 0; i < grid.length; i++ ) {
-
-    //     let tempArray = []
-      
-    //         for(let n = 0; n < grid[i].length; n++) {
-
-    //             tempArray.push(grid[n][i])
-    //         }
-
-    //     newArray.push(tempArray.join())
-    //     newArray2.push(grid[i].join())
-    // }
-
-    // newArray = [...newArray, ...newArray2]
-
-    // let counts = newArray.filter((element, index) => {
-    // return newArray.indexOf(element) == index; });
-     
-    // return newArray.length - counts.length
-
-
-
     let rows = []
     let columns = []
 
@@ -65,15 +17,12 @@ var equalPairs = function(grid) {
         let tempArray = []
       
             for(let n = 0; n < grid[i].length; n++) {
-
                 tempArray.push(grid[n][i])
             }
 
         columns.push(tempArray.join())
         rows.push(grid[i].join())
     }
-
-    
 
     for (let row of rows) {
         for (let column of columns) {
